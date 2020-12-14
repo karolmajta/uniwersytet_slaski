eb16b752c19f1d10c4d7f85434e214c6a19b6887
EC2 Default User <ec2-user@ip-172-31-16-98.eu-central-1.compute.internal>
2020-12-14 18:58:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+users = {
+    1: 'Karol Libardi',
+    2: 'John Smith',
+    3: 'Karol Marx',
+}
+
+@app.route('/')
+def index():
+    return "Index"
+
+operations = {
+    'add': lambda x1, x2: x1 + x2,
+    'subtract': lambda x1, x2: x1 - x2,
+    'multiply': lambda x1, x2: x1 * x2,
+    'divide': lambda x1, x2: x1 / x2
+}
+
+@app.route('/calc/add/', methods=['POST'])
+def calc():
+    return {"result": sum(request.json)}
+
+@app.route('/calc/pow/', methods=['POST'])
+def power():
+    result = 1
+    for x in range(0, request.json):
+        result *= x+1
+    return {"result": result}
+    
+
+@app.route('/reverse', methods = ['POST'])
+def flipText():
+    reversedText = request.json.split(' ')
+    return ' '.join(reversed(reversedText))
+    
+@app.route('/users/<int:user_id>')
+def page(user_id):
+    try:
+        return users[user_id]
+    except KeyError:
+        return "User not found...", 404
+
+@app.route('/users/<user_name>')
+def page2(user_name):
+    results = []
+    for name in users.values():
+        if user_name in name.lower():
+            results.append(name)
+    if results:
+        return ", ".join(results)
+    else:
+        return "User not found by name...", 404
+    
+
+@app.errorhandler(404) 
+def not_found(e):
+    return "Not found"
+
+app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)), debug=True)
